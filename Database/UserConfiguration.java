44b20360b77fe261f2aabee9d0627ba4fcb15c32
Avi Mot <avi@intellitradegroup.com>
2017-05-13 18:52:41 +0300

    .

--- Database/UserConfiguration.java
+++ Database/UserConfiguration.java
@@ -2,8 +2,10 @@ package Database;
 
 import Model.User;
 
+import java.awt.*;
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -11,6 +13,8 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import com.sun.prism.paint.Color;
+import javafx.scene.paint.*;
 import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -27,7 +31,6 @@ public class UserConfiguration {
 
     public static void saveUserConfig(User user){
         try {
-
 
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
