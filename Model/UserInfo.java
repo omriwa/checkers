bac1faa163c65de4a102185454112bc8e3043da2
omriwa <omriwallach1992@gmail.com>
2017-05-17 21:46:48 +0300

    UserInfo

--- Model/UserInfo.java
+++ Model/UserInfo.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
  *
  * @author omri
  */
-public class UserInfo implements Serializable{
+public class UserInfo implements Serializable{//for registration
     
     private String pass , username , color , path;
     
